9b99bb8cb98253c4770798ccd3726fbea261cb96
dcl525 <dcl525@gmail.com>
2021-06-26 15:39:38 -0700

    docker test app

--- app.py
+++ app.py
@@ -1,15 +1,1 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/test')
-def test():
-    return 'hello world'
-
-@app.route('/other-route')
-def other_route():
-    return 'this is a new route'
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
+print('hello world')
